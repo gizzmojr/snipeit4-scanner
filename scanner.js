48673ab1f25ca8cacf04d9d9429e46b37125d292
Matthew Gizzi <gizzmojr@gmail.com>
2017-09-28 18:59:10 -0400

    tabsArray is better suited as global

--- scanner.js
+++ scanner.js
@@ -4,6 +4,7 @@
 var siteUrl = "http://snipeit.msec.local";
 var apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjY5NDFmMTQ1MTk2ZTQ4M2JiM2Q5N2ViZWRhNTAwZWEzMGY4ZGFmODEwMTU5ZjUwMzYzZWI2ZWI2OWY0YzQ4MGMyZmJkYmNjNDM0ZTFlMmUzIn0.eyJhdWQiOiIzIiwianRpIjoiNjk0MWYxNDUxOTZlNDgzYmIzZDk3ZWJlZGE1MDBlYTMwZjhkYWY4MTAxNTlmNTAzNjNlYjZlYjY5ZjRjNDgwYzJmYmRiY2M0MzRlMWUyZTMiLCJpYXQiOjE0OTkxMzc0OTIsIm5iZiI6MTQ5OTEzNzQ5MiwiZXhwIjoxODE0NjcwMjkyLCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.nXL-hqofJOdcYh6xuj3Iu0hw_PLgdHaIyS8xmgAy0I-QYgylNXHv2_NQ2gD-NfYMwSpzmixZWzcfnCSHNNSXZYEJK0AU_07-TCrpfOptMxvrgepDOLzAOBJN4JYUW3C20m6Wv88heDetxkMsIO-dW0EwpZubxQnGUi_3jAUROF674MrLZkQcW9mA7f29aBpZ9GSOjg4b3IYNAwq3zmkTTZIxf6lnlx-Mv0QXbtbSkv4XqCNjAOVhWkEqPKggQKGZ0b2Rg5pkdoPXH0VbpTkRzL1bmct3saM514-14NUMWDnkwuUS7wAbf3J49m_A7mjJmZoU0lsiW7Za3iJHUKIJGoj3EaKbp9YEQZepxjqhbPFBHghvAd9G7h8ybOD4CgcDIxsTC73Yb2RRpLa7bipxioDwN-edXps-qyVk34D38icIL7YE2BNpGP5wze3QmrtZX6pkX-QQYhJzPzxnEGfso24MFA_LFSdBaV2DIgx8cWOZqkC7vCXYo60rqR2O7pxK7MhqhnbjOTfTkks4j-dabiq8zpWEjHnwk3xuM5_iOTnwors8uha2xoC6Uzj8EwnQAEhB1CGnpb5AlIJhx0Du7jYH77mlAc6GiC5V-mLjFywwHVtGR1E_Zwbf7h0_DJaKo0mk4l1ZYivPKkwgRR8qp1wRDgnlxc1eUkgn3fHtB2A";
 var mainDomElement = "#scanner";
+var tabsArray = ['Check-in', 'Check-out', 'Updating (User)', 'Updating (Location)'];
 
 // "Private" global variables. Do not touch.
 
@@ -61,25 +62,23 @@ function createLocations() {
 }
 
 function createTabs() {
-    var tabs = ['Check-in', 'Check-out', 'Updating (User)', 'Updating (Location)'];
-
     var tabsDiv = document.createElement("div");
     tabsDiv.className = "tab";
 
     document.querySelector(mainDomElement).appendChild(tabsDiv);
 
-    for (var tab in tabs) {
+    for (var tab in tabsArray) {
         var btnDiv = document.createElement("div");
-        btnDiv.id = tabs[tab];
+        btnDiv.id = tabsArray[tab];
         btnDiv.className = "tabcontent";
 
         var btn = document.createElement("button");
         btn.className = "tablinks";
-        btn.innerHTML = tabs[tab];
+        btn.innerHTML = tabsArray[tab];
         btn.addEventListener("click", function() {
             openTab(event, this.innerHTML);
         });
-        if (tabs[tab] == "Updating (User)") {
+        if (tabsArray[tab] == "Updating (User)") {
             btn.id = "defaultOpen";
         }
 
